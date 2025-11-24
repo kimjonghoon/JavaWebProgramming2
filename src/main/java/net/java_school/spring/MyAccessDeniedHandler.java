7db80c1ce54836e7cd959794e95f9626f260e206
Jonghoon Kim <kim0070@gmail.com>
2025-11-24 11:30:39 +0900

    access denied handler added

--- src/main/java/net/java_school/spring/MyAccessDeniedHandler.java
+++ src/main/java/net/java_school/spring/MyAccessDeniedHandler.java
@@ -11,17 +11,11 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 
 public class MyAccessDeniedHandler implements AccessDeniedHandler {
 
-    private String errorPage;
-
-    public void setErrorPage(String errorPage) {
-        this.errorPage = errorPage;
-    }
-
     @Override
     public void handle(HttpServletRequest req, HttpServletResponse resp, AccessDeniedException e)
             throws IOException, ServletException {
 
-        req.getRequestDispatcher(errorPage).forward(req, resp);
+		resp.sendRedirect(req.getContextPath() + "/error-403");
 
     }
 
